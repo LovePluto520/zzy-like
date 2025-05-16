7c6da823507f6965d342f9f88767536b5076d85d
LovePluto520 <strangeman613911@gmail.com>
2025-05-16 15:53:59 +0800

    第二版：用户是否点赞优化

--- src/main/java/com/example/zzylike/service/impl/ThumbServiceImpl.java
+++ src/main/java/com/example/zzylike/service/impl/ThumbServiceImpl.java
@@ -1,20 +1,28 @@
 package com.example.zzylike.service.impl;
 
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.example.zzylike.common.ErrorCode;
+import com.example.zzylike.constant.ThumbConstant;
 import com.example.zzylike.mapper.ThumbMapper;
 import com.example.zzylike.model.dto.thumb.DoThumbRequest;
+import com.example.zzylike.model.dto.thumb.ThumbInfo;
 import com.example.zzylike.model.entity.Blog;
 import com.example.zzylike.model.entity.Thumb;
 import com.example.zzylike.model.entity.User;
 import com.example.zzylike.service.BlogService;
 import com.example.zzylike.service.ThumbService;
 import com.example.zzylike.service.UserService;
+import jakarta.annotation.Resource;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.support.TransactionTemplate;
+import java.time.Instant;
 
+import java.time.temporal.ChronoUnit;
 /**
  * @author
  */
@@ -30,39 +38,76 @@ public class ThumbServiceImpl extends ServiceImpl<ThumbMapper, Thumb>
 
     private final TransactionTemplate transactionTemplate;
 
-    @Override
-    public Boolean doThumb(DoThumbRequest doThumbRequest, HttpServletRequest request) {
-        if (doThumbRequest == null || doThumbRequest.getBlogId() == null) {
-            throw new RuntimeException("参数错误");
-        }
-        User loginUser = userService.getLoginUser(request);
-        // 加锁
-        synchronized (loginUser.getId().toString().intern()) {
+    private final RedisTemplate<String, Object> redisTemplate;
 
-            // 编程式事务
-            return transactionTemplate.execute(status -> {
-                Long blogId = doThumbRequest.getBlogId();
-                boolean exists = this.lambdaQuery()
-                        .eq(Thumb::getUserId, loginUser.getId())
-                        .eq(Thumb::getBlogId, blogId)
-                        .exists();
-                if (exists) {
-                    throw new RuntimeException("用户已点赞");
-                }
-
-                boolean update = blogService.lambdaUpdate()
-                        .eq(Blog::getId, blogId)
-                        .setSql("thumbCount = thumbCount + 1")
-                        .update();
-
-                Thumb thumb = new Thumb();
-                thumb.setUserId(loginUser.getId());
-                thumb.setBlogId(blogId);
-                // 更新成功才执行
-                return update && this.save(thumb);
-            });
-        }
+//    @Override
+//    public Boolean doThumb(DoThumbRequest doThumbRequest, HttpServletRequest request) {
+//        if (doThumbRequest == null || doThumbRequest.getBlogId() == null) {
+//            throw new RuntimeException("参数错误");
+//        }
+//        User loginUser = userService.getLoginUser(request);
+//        // 加锁
+//        synchronized (loginUser.getId().toString().intern()) {
+//
+//            // 编程式事务
+//            return transactionTemplate.execute(status -> {
+//                Long blogId = doThumbRequest.getBlogId();
+//                boolean exists = this.lambdaQuery()
+//                        .eq(Thumb::getUserId, loginUser.getId())
+//                        .eq(Thumb::getBlogId, blogId)
+//                        .exists();
+//                if (exists) {
+//                    throw new RuntimeException("用户已点赞");
+//                }
+//
+//                boolean update = blogService.lambdaUpdate()
+//                        .eq(Blog::getId, blogId)
+//                        .setSql("thumbCount = thumbCount + 1")
+//                        .update();
+//
+//                Thumb thumb = new Thumb();
+//                thumb.setUserId(loginUser.getId());
+//                thumb.setBlogId(blogId);
+//                // 更新成功才执行
+//                return update && this.save(thumb);
+//            });
+//        }
+//    }
+public Boolean doThumb(DoThumbRequest doThumbRequest, HttpServletRequest request) {
+    if (doThumbRequest == null || doThumbRequest.getBlogId() == null) {
+        throw new RuntimeException("参数错误");
     }
+    User loginUser = userService.getLoginUser(request);
+    // 加锁
+    synchronized (loginUser.getId().toString().intern()) {
+
+        // 编程式事务
+        return transactionTemplate.execute(status -> {
+            Long blogId = doThumbRequest.getBlogId();
+            Boolean exists = this.hasThumb(blogId, loginUser.getId());
+            if (exists) {
+                throw new RuntimeException("用户已点赞");
+            }
+
+            boolean update = blogService.lambdaUpdate()
+                    .eq(Blog::getId, blogId)
+                    .setSql("thumb_count = thumb_count + 1")
+                    .update();
+
+            Thumb thumb = new Thumb();
+            thumb.setUserId(loginUser.getId());
+            thumb.setBlogId(blogId);
+            boolean success = update && this.save(thumb);
+
+            // 点赞记录存入 Redis
+            if (success) {
+                redisTemplate.opsForHash().put(ThumbConstant.USER_THUMB_KEY_PREFIX + loginUser.getId().toString(), blogId.toString(), thumb.getId());
+            }
+            // 更新成功才执行
+            return success;
+        });
+    }
+}
 
     @Override
     public Boolean undoThumb(DoThumbRequest doThumbRequest, HttpServletRequest request) {
@@ -76,22 +121,30 @@ public class ThumbServiceImpl extends ServiceImpl<ThumbMapper, Thumb>
             // 编程式事务
             return transactionTemplate.execute(status -> {
                 Long blogId = doThumbRequest.getBlogId();
-                Thumb thumb = this.lambdaQuery()
-                        .eq(Thumb::getUserId, loginUser.getId())
-                        .eq(Thumb::getBlogId, blogId)
-                        .one();
-                if (thumb == null) {
+                Long thumbId = ((Long) redisTemplate.opsForHash().get(ThumbConstant.USER_THUMB_KEY_PREFIX + loginUser.getId().toString(), blogId.toString()));
+                if (thumbId == null) {
                     throw new RuntimeException("用户未点赞");
                 }
                 boolean update = blogService.lambdaUpdate()
                         .eq(Blog::getId, blogId)
-                        .setSql("thumbCount = thumbCount - 1")
+                        .setSql("thumb_count = thumb_count - 1")
                         .update();
 
-                return update && this.removeById(thumb.getId());
+                boolean success = update && this.removeById(thumbId);
+
+                // 点赞记录从 Redis 删除
+                if (success) {
+                    redisTemplate.opsForHash().delete(ThumbConstant.USER_THUMB_KEY_PREFIX + loginUser.getId(), blogId.toString());
+                }
+                return success;
             });
         }
     }
+    @Override
+    public Boolean hasThumb(Long blogId, Long userId) {
+        return redisTemplate.opsForHash().hasKey(ThumbConstant.USER_THUMB_KEY_PREFIX + userId, blogId.toString());
+    }
+
 }
 
 
--- src/main/java/com/example/zzylike/service/impl/UserServiceImpl.java
+++ src/main/java/com/example/zzylike/service/impl/UserServiceImpl.java
@@ -14,7 +14,16 @@ import org.springframework.stereotype.Service;
 @Service
 public class UserServiceImpl extends ServiceImpl<UserMapper, User>
         implements UserService {
-
+    /**
+     * 从 HttpServletRequest 对象中获取当前登录用户的信息。
+     * 该方法通过 HttpServletRequest 对象获取与之关联的会话（Session），
+     * 然后从会话中获取存储的登录用户信息。
+     *
+     * @param request HttpServletRequest 对象，代表客户端的 HTTP 请求。
+     * @return 如果会话中存在登录用户信息，则返回存储的 User 对象；
+     *         如果会话中不存在登录用户信息，或者存储的对象类型不是 User 类型，
+     *         则返回 null（由于是强制类型转换，若类型不匹配会抛出 ClassCastException，此处假设调用者会处理异常）。
+     */
     @Override
     public User getLoginUser(HttpServletRequest request) {
         return (User) request.getSession().getAttribute(UserConstant.LOGIN_USER);
